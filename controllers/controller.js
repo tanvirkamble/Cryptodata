76107cf3f28743c75c770a7267b674f11eb35112
tanvir kamble <tanukamble22@gmail.com>
2023-08-09 09:45:40 +0530

    second commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const path = require('path');
 
 // Fetch data from API and store in the database
-const getfetchData = async (req, res) => {
+const get_fetchData = async (req, res) => {
   try {
     const response = await axios.get('https://api.wazirx.com/api/v2/tickers');
     const data = response.data;
@@ -12,22 +12,27 @@ const getfetchData = async (req, res) => {
     // Store data in MongoDB
     await CryptoModel.insertMany(Object.values(data));
 
-    res.send('Data fetched and stored successfully!');
+    //res.send('Data fetched and stored successfully!');
+    res.render('fetch-succes');
   } catch (error) {
     res.status(500).send('An error occurred while fetching data.');
   }
 };
 
 // Retrieve stored data from the database
-const getGetData = async (req, res) => {
+const get_GetData = async (req, res) => {
   try {
     const cryptoData = await CryptoModel.find().limit(10);
     // res.json(cryptoData);
-    res.render('index', { data: cryptoData });
+    res.render('data', { data: cryptoData });
   } catch (error) {
     res.status(500).send('An error occurred while retrieving data.');
     console.log(error);
   }
 };
 
-module.exports = { getGetData, getfetchData };
+const get_dashboard = async (req, res) => {
+  res.render('dashboard');
+};
+
+module.exports = { get_GetData, get_fetchData, get_dashboard };
--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/controller');
 
-router.get('/fetch-data', controller.getfetchData);
+router.get('/fetch-data', controller.get_fetchData);
 
-router.get('/get-data', controller.getGetData);
+router.get('/get-data', controller.get_GetData);
+
+router.get('/', controller.get_dashboard);
 
 module.exports = router;
